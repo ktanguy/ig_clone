a058371d85c41ed6c4758d9c58c6a8a0d91ff231
ktanguy <t.kwizera@alustudent.com>
2024-02-26 12:40:06 +0200

    ses

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="chrome-extension://ofpnmcalabcbjgholdjcjblkibolbppb/static/global/src/static/monicaLogo.png" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+          <div><h1>Freecodecamp</h1></div>  
+          <div class="d-flex">
+            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>23k</strong> followers</div>
+            <div class="pr-5"><strong>212</strong> following</div>
+          </div>
+          <div class="pt-4 font-weight-bold">freecodecamp.org</div>
+          <div>We're a global community of millions of people learning to <br> code together.
+LearnToCodeRPG: <br>https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
+
+        <div><a href="#">www.freecodecamp.org</a></div>
+
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://i.ebayimg.com/images/g/evAAAOSw2kFjlEuy/s-l960.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://i.ebayimg.com/images/g/n~wAAOSwAdljlEuW/s-l1600.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://i.ebayimg.com/images/g/tboAAOSw14ZjlEux/s-l960.jpg" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
